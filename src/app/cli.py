de57166f996b49ccf747d7f1eb1186a9d537cbd3
kal1nadam <kalinaadam75@seznam.cz>
2025-11-09 00:19:04 +0100

    cli commands rename

--- src/app/cli.py
+++ src/app/cli.py
@@ -1,41 +1,35 @@
 import typer
-from app.adapters.duckdb_backend import init_db, calc_purchase_attributions, get_connection_test
+from app.adapters.duckdb_backend import init_db, calc_attributions, get_connection_test, process_orders
 
 app = typer.Typer(help="CLI application for managing tasks.")
 
-@app.command()
-def run(name: str):
+@app.command("run")
+def run_cmd(name: str):
     """
     Run the main application.
     """
     typer.echo(f"App is running successfully! Hello {name}")
 
-@app.command()
-def test_db():
+@app.command("init-db")
+def init_db_cmd():
     init_db()
 
-    # total = con.execute("SELECT COUNT(*) AS total FROM events").fetchone()[0]
-    # total_attributions = con.execute("SELECT COUNT(*) AS total FROM last_click_attributions").fetchone()[0]
-    # typer.echo(f"Total events in the database: {total}")
-    # typer.echo(f"Total last click attributions in the database: {total_attributions}")
-    
-    # attributions = con.execute(
-    #     """
-    #     SELECT * FROM last_click_attributions LIMIT 100;
-    #     """).fetchdf()
-    
-    # typer.echo(attributions)
-    
-@app.command()
-def calc_attributions():
-    calc_purchase_attributions()
 
-@app.command()
-def test():
-    con = get_connection_test()
-    typer.echo(con.execute("SELECT * FROM events LIMIT 10").fetchdf()) 
-    # con.execute("DELETE FROM events")
-    # con.execute("DELETE FROM purchase_last_click_attributions")
+@app.command("calc-attributions")
+def calc_attributions_cmd():
+    calc_attributions()
+
+@app.command("test")
+def test_cmd():
+    print("Testing...")
+    # con = get_connection_test()
+    # typer.echo(con.execute("SELECT * FROM events LIMIT 10").fetchdf()) 
+    # con.execute("DROP TABLE events")
+    # con.execute("DROP TABLE purchase_last_click_attributions")
+    # con.execute("DROP VIEW events_seed;")
+
+    # orders = process_orders()
+
 
 if __name__ == "__main__":
     app()
