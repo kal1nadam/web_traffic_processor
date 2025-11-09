3e436b84a7693a8c22f7fe4ae96edfc67308c944
kal1nadam <kalinaadam75@seznam.cz>
2025-11-09 14:07:28 +0100

    init schema sql update - create orders table - create products table

--- src/app/adapters/sql/init_schema.sql
+++ src/app/adapters/sql/init_schema.sql
@@ -7,12 +7,25 @@ CREATE TABLE IF NOT EXISTS events (
         items STRUCT(item_id VARCHAR, item_name VARCHAR, item_brand VARCHAR, item_variant VARCHAR, item_category VARCHAR, item_category2 VARCHAR, item_category3 VARCHAR, item_category4 VARCHAR, item_category5 VARCHAR, price_in_usd DOUBLE, price DOUBLE, quantity BIGINT, item_revenue_in_usd DOUBLE, item_revenue DOUBLE, item_refund_in_usd DOUBLE, item_refund DOUBLE, coupon VARCHAR, affiliation VARCHAR, location_id VARCHAR, item_list_id VARCHAR, item_list_name VARCHAR, item_list_index VARCHAR, promotion_id VARCHAR, promotion_name VARCHAR, creative_name VARCHAR, creative_slot VARCHAR, item_params STRUCT("key" VARCHAR, "value" STRUCT(string_value VARCHAR, int_value BIGINT, float_value DOUBLE, double_value DOUBLE))[])[]
     );
 
-    CREATE TABLE IF NOT EXISTS orders (
-        order_id UUID PRIMARY KEY
+    CREATE TABLE orders (
+        id                UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+        event_timestamp   TIMESTAMP NOT NULL,
+        hostname          VARCHAR NOT NULL,
+        user_pseudo_id    VARCHAR NOT NULL,
+        currency          VARCHAR,
+        value             DOUBLE,
+        source            VARCHAR,
+        medium            VARCHAR,
+        campaign          VARCHAR
     );
 
-    CREATE TABLE IF NOT EXISTS products (
-        product_id UUID PRIMARY KEY
+    CREATE TABLE products (
+        id          UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+        order_id    UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+        feed_id     VARCHAR NOT NULL,
+        name        VARCHAR NOT NULL,
+        price       DOUBLE,
+        quantity    INTEGER
     );
 
     CREATE TABLE IF NOT EXISTS purchase_last_click_attributions (
